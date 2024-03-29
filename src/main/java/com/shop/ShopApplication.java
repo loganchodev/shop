fc821c5483646111cae0e9956e5d5312daeb87a2
loganchodev <loganchodev@gmail.com>
2024-03-29 11:44:42 +0900

    application.yml 변환

--- src/main/java/com/shop/ShopApplication.java
+++ src/main/java/com/shop/ShopApplication.java
@@ -2,12 +2,26 @@ package com.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ShopApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ShopApplication.class, args);
 	}
+}
 
+@RestController
+class HiController {
+
+	@GetMapping("/hi")
+	public String sayHi() {
+		return "hi";
+	}
+
+	@GetMapping("/")
+	public String home() {
+		return "Welcome to the home page!";
+	}
 }
